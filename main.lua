7af6b3b6bc7e809c897903c6cfc0fc88ebae813c
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-10-19 09:00:54 +0100

    Create deck and initial player hand

--- main.lua
+++ main.lua
@@ -1,6 +1,23 @@
 -- require('src.utils.debug')
 
 function love.load()
+    deck = {}
+    for suitIndex, suit in ipairs({ 'club', 'diamond', 'heart', 'spade' }) do
+        for rank = 1, 13 do
+            print('suit: '..suit..', rank: '..rank)
+            table.insert(deck, { suit = suit, rank = rank })
+        end
+    end
+
+    playerHand = {}
+    table.insert(playerHand, table.remove(deck, love.math.random(#deck)))
+    table.insert(playerHand, table.remove(deck, love.math.random(#deck)))
+    
+    print('Player hand:')
+    for cardIndex, card in ipairs(playerHand) do
+        print('suit: '..card.suit..', rank: '..card.rank)
+    end
+    print('Total number of cards in deck: '..#deck)
 
 end
 
@@ -9,7 +26,14 @@ function love.update(dt)
 end
 
 function love.draw()
+    local output = {}
 
+    table.insert(output, 'Player hand:')
+    for cardIndex, card in ipairs(playerHand) do
+        table.insert(output, 'suit: '..card.suit..', rank: '..card.rank)
+    end
+
+    love.graphics.print(table.concat(output, '\n'), 15, 15)
 end
 
 function love.keypressed(key)
