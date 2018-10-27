fd67f581b2da6a0f2a56f6f99f862b3fb894f1b1
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-10-27 17:41:15 +0100

    Draw ALL the pips!

--- main.lua
+++ main.lua
@@ -45,6 +45,15 @@ function drawCard(card, x, y)
     love.graphics.setColor(1, 1, 1)
     love.graphics.draw(images.card, x, y)
 
+    if card.suit == 'heart' or card.suit == 'diamond' then
+        love.graphics.setColor(0.89, 0.06, 0.39)
+    else
+        love.graphics.setColor(0.2, 0.2, 0.2)
+    end
+
+    drawCorner(images[card.rank], numberOffsetX, numberOffsetY)
+    drawCorner(suitImage, suitOffsetX, suitOffsetY)
+
     if card.rank > 10 then
         local faceImage
         if card.rank == 11 then
@@ -56,17 +65,59 @@ function drawCard(card, x, y)
         end
         love.graphics.setColor(1, 1, 1)
         love.graphics.draw(faceImage, x + 12, y + 11)
-    end
-
-    if card.suit == 'heart' or card.suit == 'diamond' then
-        love.graphics.setColor(0.89, 0.06, 0.39)
     else
-        love.graphics.setColor(0.2, 0.2, 0.2)
+        local function drawPip(offsetX, offsetY, mirrorX, mirrorY)
+            local pipImage = images['pip_' .. card.suit]
+            local pipWidth = 11
+
+            love.graphics.draw(pipImage, x + offsetX, y + offsetY)
+            if mirrorX then
+                love.graphics.draw(pipImage, x + cardWidth - offsetX - pipWidth, y + offsetY)
+            end
+            if mirrorY then
+                love.graphics.draw(pipImage, x + offsetX + pipWidth, y + cardHeight - offsetY, 0, -1, -1)
+            end
+            if mirrorX and mirrorY then
+                love.graphics.draw(pipImage, x + cardWidth - offsetX, y + cardHeight - offsetY, 0, -1, -1)
+            end
+        end
+
+        local xLeft, xMid = 11, 21
+        local yTop, yThird, yQtr, yMid = 7, 19, 23, 31
+
+        if card.rank == 1 then
+            drawPip(xMid, yMid)
+        elseif card.rank == 2 then
+            drawPip(xMid, yTop, false, true)
+        elseif card.rank == 3 then
+            drawPip(xMid, yTop, false, true)
+            drawPip(xMid, yMid)
+        elseif card.rank == 4 then
+            drawPip(xLeft, yTop, true, true)
+        elseif card.rank == 5 then
+            drawPip(xLeft, yTop, true, true)
+            drawPip(xMid, yMid)
+        elseif card.rank == 6 then
+            drawPip(xLeft, yTop, true, true)
+            drawPip(xLeft, yMid, true)
+        elseif card.rank == 7 then
+            drawPip(xLeft, yTop, true, true)
+            drawPip(xLeft, yMid, true)
+            drawPip(xMid, yThird)
+        elseif card.rank == 8 then
+            drawPip(xLeft, yTop, true, true)
+            drawPip(xLeft, yMid, true)
+            drawPip(xMid, yThird, false, true)
+        elseif card.rank == 9 then
+            drawPip(xLeft, yTop, true, true)
+            drawPip(xLeft, yQtr, true, true)
+            drawPip(xMid, yMid)
+        elseif card.rank == 10 then
+            drawPip(xLeft, yTop, true, true)
+            drawPip(xLeft, yQtr, true, true)
+            drawPip(xMid, 16, false, true)
+        end
     end
-
-
-    drawCorner(images[card.rank], numberOffsetX, numberOffsetY)
-    drawCorner(suitImage, suitOffsetX, suitOffsetY)
 end
 
 
